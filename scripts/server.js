f290d96963b334a75ff3686339397710fc7ffbb3
darkacer <omkardeokar95@gmail.com>
2020-05-03 14:58:23 +0530

    works on my machine

--- scripts/server.js
+++ scripts/server.js
@@ -18,6 +18,16 @@ app.use('*', (req, res) => {
     res.sendFile(path.resolve(DIST_DIR, 'index.html'));
 });
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
     console.log(`✅  Server started: http://${HOST}:${PORT}`)
 );
+const socketIO = require('socket.io')(server);
+//const io = socketIO(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+
+    socket.on('user_join', (data) => console.log('data is ' + data.message));
+});
--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -1,3 +1,11 @@
 import { LightningElement } from 'lwc';
+import io from 'socket.io-client';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    handleSend() {
+        console.log('someone clicked!');
+        const socket = io();
+
+        socket.emit('user_join', { message: 'whats up!' });
+    }
+}
